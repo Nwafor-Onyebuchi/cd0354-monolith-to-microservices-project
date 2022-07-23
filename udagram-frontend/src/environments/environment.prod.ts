1ed7d4e7cb847f39cb5d9cd682b46a174dc988db
Nwafor-Onyebuchi <buchilazarus4@gmail.com>
2022-07-23 14:01:25 +0100

    expose public ip

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac13638f6041f453da94f918901c5728-1829151716.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
